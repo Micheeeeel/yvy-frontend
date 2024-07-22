0a7910bb14fc5ae10363ebda7c09a222fd2cb527
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-22 14:13:56 +0200

    Create my first component : FrameLandingPage

---
+++ src/components/FrameLandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FrameLandingPage = () => {
+  return (
+    <div className="frame-landing-page">
+      <h1>Welcome to Frame Landing Page</h1>
+    </div>
+  );
+};
+
+export default FrameLandingPage;
