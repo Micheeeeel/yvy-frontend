74e26e57013a62dd923f9c63130844e27407190e
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-24 13:09:24 +0200

    Configuration of Tailwind, but keeping former css files for the moment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {
-    extend: {},
+export const content = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const theme = {
+  extend: {
+    colors: {
+      alert: "rgba(191, 33, 30, 1)",
+      black: "rgba(0, 15, 8, 1)",
+      primary: "rgba(58, 119, 81, 1)",
+      secondary: "rgba(240, 135, 0, 1)",
+      ternary: "rgba(239, 202, 8, 1)",
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
