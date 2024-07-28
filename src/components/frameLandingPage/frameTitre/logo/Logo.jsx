2cdc5f0f04e86d30a0fe15a07105912aa9b9feee
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-28 19:26:36 +0200

    Refactor Logo component: Update className for logo to convert it for TailwindCss

--- src/components/frameLandingPage/frameTitre/logo/Logo.jsx
+++ src/components/frameLandingPage/frameTitre/logo/Logo.jsx
@@ -1,11 +1,10 @@
 // src/components/Logo.jsx
 import React from "react";
-import "./Logo.css";
 
 const Logo = () => {
   return (
     <svg
-      className="frame-titre-logo"
+      className="frame-titre-logo w-[203px] h-auto mb-5"
       viewBox="0 0 745 240"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -15,120 +14,117 @@ const Logo = () => {
         cy="115"
         r="115"
         transform="matrix(-1 0 0 1 603 5)"
-        fill="#FFFFFE"
-        stroke="var(--black)"
         strokeWidth="10"
+        className="fill-white stroke-black"
       />
       <path
         d="M488 220C461.478 220 436.043 209.464 417.289 190.711C398.536 171.957 388 146.522 388 120C388 93.4783 398.536 68.043 417.289 49.2893C436.043 30.5357 461.478 20 488 20V120L488 220Z"
-        className="primary-fill"
+        className="fill-primary"
       />
       <circle
         cx="62.5"
         cy="62.5"
         r="67.5"
         transform="matrix(-1 0 0 1 550 95)"
-        className="black-fill"
-        stroke="white"
+        className="fill-black stroke-white"
         strokeWidth="10"
       />
       <path
         d="M488 220C514.522 220 539.957 209.464 558.711 190.711C577.464 171.957 588 146.522 588 120C588 93.4783 577.464 68.043 558.711 49.2893C539.957 30.5357 514.522 20 488 20V120L488 220Z"
-        className="black-fill"
+        className="fill-black"
       />
       <circle
         cx="17.5"
         cy="17.5"
         r="17.5"
         transform="matrix(-1 0 0 1 506 140)"
-        fill="white"
+        className="fill-white"
       />
       <circle
         cx="32.5"
         cy="32.5"
         r="32.5"
         transform="matrix(-1 0 0 1 521 20)"
-        className="primary-fill"
+        className="fill-primary"
       />
       <circle
         cx="9.23684"
         cy="9.23684"
         r="9.23684"
         transform="matrix(-1 0 0 1 497.474 43.5264)"
-        className="black-fill"
+        className="fill-black"
       />
       <path
         d="M487.5 15C497.446 15 506.984 18.9509 514.017 25.9835C521.049 33.0161 525 42.5544 525 52.5C525 62.4456 521.049 71.9839 514.017 79.0165C506.984 86.0491 497.446 90 487.5 90"
-        stroke="white"
         strokeWidth="10"
+        className="stroke-white"
       />
       <path
         d="M621 80H711"
-        className="secondary-stroke"
         strokeWidth="15"
         strokeLinecap="round"
+        className="stroke-secondary"
       />
       <path
         d="M621 161H711"
-        className="secondary-stroke"
+        className="stroke-secondary"
         strokeWidth="15"
         strokeLinecap="round"
       />
       <path
         d="M629 120H737"
-        className="secondary-stroke"
         strokeWidth="15"
         strokeLinecap="round"
+        className="stroke-secondary"
       />
       <circle
         cx="259"
         cy="120"
         r="115"
-        fill="#FFFFFE"
-        stroke="var(--black)"
         strokeWidth="10"
+        className="fill-white stroke-black"
       />
       <path
         d="M259 220C285.522 220 310.957 209.464 329.711 190.711C348.464 171.957 359 146.522 359 120C359 93.4783 348.464 68.043 329.711 49.2893C310.957 30.5357 285.522 20 259 20V120L259 220Z"
-        className="black-fill"
+        className="fill-black"
       />
       <circle
         cx="259.5"
         cy="157.5"
         r="67.5"
-        className="primary-fill"
-        stroke="white"
         strokeWidth="10"
+        className="fill-primary stroke-white"
       />
       <path
         d="M259 220C232.478 220 207.043 209.464 188.289 190.711C169.536 171.957 159 146.522 159 120C159 93.4783 169.536 68.043 188.289 49.2893C207.043 30.5357 232.478 20 259 20V120L259 220Z"
-        className="primary-fill"
+        className="fill-primary"
       />
-      <circle cx="259.5" cy="160.5" r="17.5" className="black-fill" />
-      <circle cx="259.5" cy="52.5" r="32.5" className="black-fill" />
+      <circle cx="259.5" cy="160.5" r="17.5" className="fill-black" />
+      <circle cx="259.5" cy="52.5" r="32.5" className="fill-black" />
       <circle cx="259.237" cy="52.7632" r="9.23684" fill="white" />
       <path
         d="M259.5 15C249.554 15 240.016 18.9509 232.983 25.9835C225.951 33.0161 222 42.5544 222 52.5C222 62.4456 225.951 71.9839 232.983 79.0165C240.016 86.0491 249.554 90 259.5 90"
         stroke="white"
         strokeWidth="10"
+        className="stroke-white"
       />
       <path
         d="M124 80H34"
-        className="secondary-stroke"
         strokeWidth="15"
         strokeLinecap="round"
+        className="stroke-secondary"
       />
       <path
         d="M124 161H34"
-        className="secondary-stroke"
         strokeWidth="15"
         strokeLinecap="round"
+        className="stroke-secondary"
       />
       <path
         d="M116 120H8"
-        className="secondary-stroke"
         strokeWidth="15"
         strokeLinecap="round"
+        className="stroke-secondary"
       />
     </svg>
   );
