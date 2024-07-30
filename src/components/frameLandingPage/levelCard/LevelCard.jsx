8cff5ab14e031285aad152f2e35842f87782643b
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-30 23:04:05 +0200

    Creation of the componant for the levelCard. Creation of the gradients for card borders.

--- src/components/frameLandingPage/levelCard/LevelCard.jsx
+++ src/components/frameLandingPage/levelCard/LevelCard.jsx
@@ -1,20 +1,27 @@
+// src/components/LevelCard.jsx
 import React from "react";
 import CardLinesDesktop from "../CardLinesDesktop";
+import "../../styles/gradients.css"; // Assurez-vous d'importer les styles
+import image from "../../../assets/images/eco-guards-im-regenwald-mathias-rittgerott-rdr 1.png";
 
 const LevelCard = () => {
   return (
-    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className="flex flex-col p-4 gap-4">
-        <h2 className="text-xl font-semibold text-gray-800 text-left">
-          John Doe
-        </h2>
-        <CardLinesDesktop color="border-secondary" />
-        <img
-          className="w-full h-48 object-cover object-center "
-          src="https://via.placeholder.com/300"
-          alt="Profile"
-        />
-        <p className="mt-2 text-gray-600 text-left">
+    <div className="relative max-w-[430px] h-[510px] mx-auto bg-black shadow-lg overflow-hidden gradient-border">
+      <div className="relative flex flex-col p-4 gap-4 ">
+        <div>
+          <h2 className="text-xl font-semibold text-white text-left">
+            John Doe
+          </h2>
+          <CardLinesDesktop color="border-secondary" />
+        </div>
+        <div className="flex justify-center">
+          <img
+            className="w-full h-48 object-cover"
+            src={image}
+            alt="niveau jouable"
+          />
+        </div>
+        <p className="text-white text-left ">
           A passionate developer who loves coding and enjoys learning new
           technologies.
         </p>
