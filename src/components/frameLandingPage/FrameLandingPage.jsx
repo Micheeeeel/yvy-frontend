7dde19caf3783e633084fcc6fb9333718fd15344
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-31 15:58:51 +0200

    Make FrameLandingPage responsive and fluid
    - Replaced fixed width with `w-full` and set a maximum width using `max-w-screen-xl` to allow the page to scale fluidly with the window size.
    - Centered content using `mx-auto` to ensure alignment across different screen sizes.
    - Adjusted `ImagePresentation` to scale proportionally with `w-full` and `h-auto`, ensuring it maintains its aspect ratio.
    - Set the container for `LevelCard` components to `w-full` and enabled `flex-wrap` for wrapping cards as needed, maintaining a responsive layout.

--- src/components/frameLandingPage/FrameLandingPage.jsx
+++ src/components/frameLandingPage/FrameLandingPage.jsx
@@ -6,12 +6,14 @@ import LevelCard from "./levelCard/LevelCard";
 
 const FrameLandingPage = () => {
   return (
-    <div className="bg-black text-white w-[1440px] min-h-screen relative">
-      <div className="flex flex-col items-center gap-5 w-[1440px] pt-20 pb-20   ">
+    <div className="bg-black text-white w-full min-h-screen relative">
+      <div className="flex flex-col items-center gap-5 w-full max-w-screen-xl mx-auto pt-20 pb-20   ">
         <FrameTitre />
-        <ImagePresentation />
+        <div className="w-full px-4">
+          <ImagePresentation className=" h-auto object-contain " />
+        </div>
         <CardLinesDesktop color="border-primary" />
-        <div className="flex flex-wrap gap-x-[32px] gap-y-[32px] px-[32px] py-[32px] justify-center">
+        <div className="flex flex-wrap w-full gap-x-[32px] gap-y-[32px] px-[32px] py-[32px] ">
           <LevelCard />
           <LevelCard />
           <LevelCard />
