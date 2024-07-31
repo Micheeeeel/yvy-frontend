8d2746eb3c950f828961748812184e18d6cb5ca5
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-07-31 14:43:35 +0200

    Refactor container layout: Remove fixed height to allow dynamic resizing based on content
    - Removed fixed height (`h-[2572px]`) from the card container to allow it to expand based on its content.
    - Ensured the container can adapt to the size of its content by utilizing `min-h-screen` for the main wrapper.
    - Switched positioning from `absolute` to `relative` to maintain the container in the normal document flow and support dynamic height adjustments.

--- src/components/frameLandingPage/FrameLandingPage.jsx
+++ src/components/frameLandingPage/FrameLandingPage.jsx
@@ -6,12 +6,14 @@ import LevelCard from "./levelCard/LevelCard";
 
 const FrameLandingPage = () => {
   return (
-    <div className="bg-black text-white w-[1440px] h-[2400px]  relative">
-      <div className="flex flex-col items-center gap-5 w-[1440px] h-[2572px]  pt-20 pb-20 absolute  ">
+    <div className="bg-black text-white w-[1440px] min-h-screen relative">
+      <div className="flex flex-col items-center gap-5 w-[1440px] pt-20 pb-20   ">
         <FrameTitre />
         <ImagePresentation />
         <CardLinesDesktop color="border-primary" />
         <div className="flex flex-wrap gap-x-[32px] gap-y-[32px] px-[32px] py-[32px] justify-center">
+          <LevelCard />
+          <LevelCard />
           <LevelCard />
           <LevelCard />
           <LevelCard />
