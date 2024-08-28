a395f024c606e109ed7afdcbe1e9297c9232dd66
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2024-08-28 13:40:13 +0200

    feat: Add smooth transitions between components PresenationImage and LevelesMap using CSSTransition
    - Integrated `react-transition-group` to enable smooth transitions between components.
    - Utilized `CSSTransition` and `TransitionGroup` to animate the transition between child components of `Accueil` based on the route change.
    - Imported the `transitions.css` file to define CSS rules for fade-in and fade-out animations.
    - Applied `useLocation` to track the current route path for triggering the correct transitions.

--- src/routes/Accueil.tsx
+++ src/routes/Accueil.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import TitleSection from "../components/titleSection/TitleSection";
 import CardDivider from "../components/share/CardDivider";
 import LevelCard from "../components/levelCard/LevelCard";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "../components/styles/transitions.css"; // Fichier CSS pour les transitions entre composants
 
 const Accueil = () => {
+  const location = useLocation(); // Récupère l'emplacement actuel
+
   return (
     <div className="max-w-screen-3xl mx-auto pt-20 pb-20 px-4">
       <TitleSection />
-      <div>
-        <Outlet />
-      </div>
+      <TransitionGroup>
+        {/* Utilisation de CSSTransition pour appliquer une animation à l'Outlet */}
+        <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+          <div>
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
       <CardDivider color="border-primary" />
       <div className="flex flex-wrap gap-[32px] p-[8px] justify-center">
         <div className="flex justify-center w-[calc(100%-32px)] sm:w-[calc(50%-32px)] lg:w-[calc(33.33%-32px)] xl:w-[calc(25%-32px)]">
